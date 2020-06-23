36efac73c99dceefd19b4ee53c4e25898350e503
Anuj.Srivastava3 <Anuj.Srivastava3@IN2448833W2>
2020-06-23 12:06:37 +0530

    Created DataHelper

--- EATestProject/Helpers/DataHelperExtension.cs
+++ EATestProject/Helpers/DataHelperExtension.cs
@@ -1,12 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace EAAutoFramework.Helpers
 {
-    class DataHelperExtension
+    public static class DataHelperExtension
     {
+        //Open Connection
+        public static SqlConnection DBConnect(this SqlConnection sqlConnection, string connectionString)
+        {
+            try
+            {
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+                return sqlConnection;
+            }
+            catch (Exception e )
+            {
+                LogHelpers.Write("ERROR :: "+ e.Message);
+            }
+
+            return null;
+        }
+
+        //Closing Connection
+        public static void DBClose(this SqlConnection sqlConnection)
+        {
+            try
+            {
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+
+                LogHelpers.Write("ERROR :: " + e.Message);
+            }
+        }
+
+        //Executing Query
+        public static DataTable ExecuteQuery(this SqlConnection sqlConnection, string queryString)
+        {
+            DataSet dataSet;
+            try
+            {
+                //Checking state of connection
+                if(sqlConnection==null || ((sqlConnection!=null && (sqlConnection.State == ConnectionState.Closed
+                    || sqlConnection.State==ConnectionState.Broken))))
+                {
+                    sqlConnection.Open();
+                }
+
+                SqlDataAdapter dataAdapter = new SqlDataAdapter();
+                dataAdapter.SelectCommand = new SqlCommand(queryString, sqlConnection);
+                dataAdapter.SelectCommand.CommandType = CommandType.Text;
+
+                dataSet = new DataSet();
+                dataAdapter.Fill(dataSet,"table");
+                sqlConnection.Close();
+                return dataSet.Tables["table"];
+            }
+            catch (Exception e)
+            {
+                dataSet = null;
+                sqlConnection.Close();
+                LogHelpers.Write("ERROR :: " + e.Message);
+                return null;
+            }
+            finally
+            {
+                sqlConnection.Close();
+                dataSet = null;
+            }
+
+        }
     }
 }
